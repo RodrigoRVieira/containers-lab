1aa00a37d3795e80aa35a6244037190758b32740
Rodrigo Riccitelli Vieira <mcbacker@gmail.com>
2019-08-07 19:32:59 -0300

    WIP

--- 3.Swarm/2.Iniciar_modo_Swarm_com_IP_publico.sh
+++ 3.Swarm/2.Iniciar_modo_Swarm_com_IP_publico.sh
@@ -1,3 +1,13 @@
+### Demo Setup
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 #Initialize a Swarm with docker swarm init
+
+clear >$(tty)
+
+printf "Make sure ${RED}2377,7946,4789${NC} ports are open in Azure Portal\n\n"
 
 docker swarm init --advertise-addr $(dig +short myip.opendns.com @resolver1.opendns.com)
